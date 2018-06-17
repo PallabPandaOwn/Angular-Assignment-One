c119af6c13c0635e2a610026297dd98997932fd0
pallab.panda@heatsoftware.com <pallab.panda@heatsoftware.com>
2018-06-17 21:09:16 +0530

    Added 2 component and displayed on broswerfinished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Warningalert } from '../WarningAlert/WarningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,Warningalert, SuccessAlertComponent
   ],
   imports: [
     BrowserModule
